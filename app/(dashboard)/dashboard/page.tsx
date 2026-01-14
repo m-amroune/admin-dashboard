5e786047ce062547f8e6f91a9182f885696f6575
Moustapha Amroune <amroune.moustapha@gmail.com>
2026-01-14 14:06:29 +0100

    chore : add logout handling

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+async function logout() {
+  "use server";
+
+  const cookieStore = await cookies();
+  cookieStore.delete("dh_session");
+
+  redirect("/login");
+}
+
 const page = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <form action={logout}>
+        <button type="submit">Logout</button>
+      </form>
+    </div>
+  );
 };
 
 export default page;
