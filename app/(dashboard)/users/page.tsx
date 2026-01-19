3b1be21c5d2eb3282b6d6cd1864fb2196401daff
Moustapha Amroune <amroune.moustapha@gmail.com>
2026-01-19 14:26:23 +0100

    add user deletion

--- app/(dashboard)/users/page.tsx
+++ app/(dashboard)/users/page.tsx
@@ -8,6 +8,12 @@ async function createUser(formData: FormData) {
   "use server";
   redirect("/users?submitted=1");
 }
+// Server action to delete a user by id
+async function deleteUser(formData: FormData) {
+  "use server";
+  const id = Number(formData.get("id"));
+  await prisma.user.delete({ where: { id } });
+}
 
 export default async function Page() {
   // Fetch all users from the database
@@ -19,7 +25,13 @@ export default async function Page() {
 
       {/* Render a list of user emails */}
       {users.map((u) => (
-        <div key={u.id}>{u.email}</div>
+        <div key={u.id}>
+          {u.email}
+          <form action={deleteUser}>
+            <input type="hidden" name="id" value={u.id} />
+            <button type="submit">Delete</button>
+          </form>
+        </div>
       ))}
       <form action={createUser}>
         <input type="email" name="email" placeholder="Email" />
